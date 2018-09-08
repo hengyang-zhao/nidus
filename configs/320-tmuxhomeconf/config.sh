988a7ce6e116b3c952a849fdc4d9a674f5a3a7fb
Hengyang Zhao <hzhao877502@gmail.com>
2018-09-08 12:37:12 -0700

    Add tmux install

---
+++ configs/320-tmuxhomeconf/config.sh
@@ -0,0 +1,8 @@
+type="copy"
+source="tmux.conf"
+conflict="backup"
+backup_name="tmuxhomeconf"
+install="$HOME/.tmux.conf"
+
+# vim: ft=sh:
+
---
+++ configs/330-tmuxsiteconf/config.sh
@@ -0,0 +1,7 @@
+type="copy"
+source="custom.conf"
+conflict="skip"
+install="$SITE_DIR/tmux/custom.conf"
+
+# vim: ft=sh:
+
