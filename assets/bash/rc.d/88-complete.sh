94dfd741699530ba1fb196f68a049a33970ea52c
Hengyang Zhao <hengyangz@google.com>
2019-04-14 04:54:06 +0000

    Complete commands lab and bashtrap

--- assets/bash/rc.d/88-complete.sh
+++ assets/bash/rc.d/88-complete.sh
@@ -40,3 +40,34 @@ __nidus_complete()
     esac
 }
 complete -F __nidus_complete nidus
+
+__nidus_list_hook_labels()
+{
+    (
+        declare -F
+    ) | (
+        regex='^declare -f nidus_hook_label_(.*)$'
+        while read line; do
+            if [[ "$line" =~ $regex ]]; then
+                echo "${BASH_REMATCH[1]}"
+            fi
+        done
+    )
+}
+
+__nidus_infinite_bash_complete()
+{
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+
+    case "${#COMP_WORDS[@]}" in
+        2)
+            local opts="$(__nidus_list_hook_labels)"
+            COMPREPLY=( $(compgen -W "${opts[*]}" -- "${cur}") )
+            ;;
+        *)
+            COMPREPLY=()
+    esac
+}
+complete -F __nidus_infinite_bash_complete __nidus_infinite_bash
+complete -F __nidus_infinite_bash_complete lab
+complete -F __nidus_infinite_bash_complete bashtrap
